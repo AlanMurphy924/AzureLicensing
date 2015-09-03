b5186c77f1871cc9b3ff309c8479951aa63e0bf3
alan <T4nger!ne>
2015-09-03 14:22:44 +0100

    Moved column names of mappers to constants

--- Licensing.Mappers/CompanyMapper.cs
+++ Licensing.Mappers/CompanyMapper.cs
@@ -8,58 +8,74 @@ using System.Web;
 
 namespace AzureLicensing.Mappers
 {
+    /// <summary>
+    /// Maps POCO fields for Company to column names in database for Entity Framework
+    /// </summary>
     public class CompanyMapper : EntityTypeConfiguration<Company>
     {
+        private const string CompanyTable = "Company";
+        private const string CompanyIdColumn = "Id";
+        private const string CompanyNameColumn = "CompanyName";
+        private const string ConsignorNameColumn = "ConsignorName";
+        private const string ConsignorAdditionalField1Column = "ConsignorAdd1";
+        private const string ConsignorAdditionalField2Column = "ConsignorAdd2";
+        private const string ConsignorAdditionalField3Column = "ConsignorAdd3";
+        private const string PinColumn = "Pin";
+        private const string ColossusRegistrationNumberColumn = "ColossusRegNo";
+        private const string ColossusDesktopLicensesColumn = "ColossusDesktopLicenses";
+        private const string ColossusMobileLicensesColumn = "ColossusMobileLicenses";
+        private const string ColossusMobileUrlColumn = "ColossusMobileUrl";
+            
         public CompanyMapper()
         {
             // Map object to the db table
-            this.ToTable("Company");
+            this.ToTable(CompanyTable);
 
             // Mapping for the CompanyId
-            this.Property(c => c.CompanyId).HasColumnName("Id");
+            this.Property(c => c.CompanyId).HasColumnName(CompanyIdColumn);
             this.HasKey(c => c.CompanyId);
             this.Property(c => c.CompanyId).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
             this.Property(c => c.CompanyId).IsRequired();
 
             // Mapping for the CompanyName
-            this.Property(c => c.CompanyName).HasColumnName("CompanyName");
+            this.Property(c => c.CompanyName).HasColumnName(CompanyNameColumn);
             this.Property(c => c.CompanyName).IsRequired();
 
             // Mapping for ConsignorName
-            this.Property(c => c.ConsignorName).HasColumnName("ConsignorName");
+            this.Property(c => c.ConsignorName).HasColumnName(ConsignorNameColumn);
             this.Property(c => c.ConsignorName).IsRequired();
 
             // Mapping for ConsignorAdd1
-            this.Property(c => c.ConsignorAdd1).HasColumnName("ConsignorAdd1");
+            this.Property(c => c.ConsignorAdd1).HasColumnName(ConsignorAdditionalField1Column);
             this.Property(c => c.ConsignorAdd1).IsRequired();
 
             // Mapping for ConsignorAdd2
-            this.Property(c => c.ConsignorAdd2).HasColumnName("ConsignorAdd2");
+            this.Property(c => c.ConsignorAdd2).HasColumnName(ConsignorAdditionalField2Column);
             this.Property(c => c.ConsignorAdd2).IsRequired();
 
             // Mapping for ConsignorAdd13
-            this.Property(c => c.ConsignorAdd3).HasColumnName("ConsignorAdd3");
+            this.Property(c => c.ConsignorAdd3).HasColumnName(ConsignorAdditionalField3Column);
             this.Property(c => c.ConsignorAdd3).IsRequired();
 
             // Mapping for PIN
-            this.Property(c => c.Pin).HasColumnName("Pin");
+            this.Property(c => c.Pin).HasColumnName(PinColumn);
             this.Property(c => c.Pin).IsRequired();
             //this.Property(c => c.Pin).HasMaxLength(8);
 
             // Mapping for ColossusRegNo
-            this.Property(c => c.ColossusRegNo).HasColumnName("ColossusRegNo");
+            this.Property(c => c.ColossusRegNo).HasColumnName(ColossusRegistrationNumberColumn);
             this.Property(c => c.ColossusRegNo).IsRequired();
 
             // Mapping for ColossusDesktopLicenses
-            this.Property(c => c.ColossusDesktopLicences).HasColumnName("ColossusDesktopLicences");
+            this.Property(c => c.ColossusDesktopLicences).HasColumnName(ColossusDesktopLicensesColumn);
             this.Property(c => c.ColossusDesktopLicences).IsRequired();
 
             // Mapping for ColossusMobileLicenses
-            this.Property(c => c.ColossusMobileLicences).HasColumnName("ColossusMobileLicences");
+            this.Property(c => c.ColossusMobileLicences).HasColumnName(ColossusMobileLicensesColumn);
             this.Property(c => c.ColossusMobileLicences).IsRequired();
 
             // Mapping for ColossusMobileUrl
-            this.Property(c => c.ColossusMobileUrl).HasColumnName("ColossusMobileUrl");
+            this.Property(c => c.ColossusMobileUrl).HasColumnName(ColossusMobileUrlColumn);
             this.Property(c => c.ColossusMobileUrl).IsRequired();
         }
     }
--- Licensing.Mappers/MobileDeviceMapper.cs
+++ Licensing.Mappers/MobileDeviceMapper.cs
@@ -8,29 +8,38 @@ using System.Web;
 
 namespace AzureLicensing.Mappers
 {
+    /// <summary>
+    /// Maps POCO fields for MobileDevice to column names in database for Entity Framework
+    /// </summary>
     public class MobileDeviceMapper : EntityTypeConfiguration<MobileDevice>
     {
+        private const string MobileDeviceTable = "MobileDevice";
+        private const string MobileDeviceIdColumn = "Id";
+        private const string SerialNumberColumn = "SerialNo";
+        private const string CreatedColumn = "Created";
+        private const string CompanyIdColumn = "CompanyId";
+
         public MobileDeviceMapper()
         {
             // Map object to the db table
-            this.ToTable("MobileDevice");
+            this.ToTable(MobileDeviceTable);
 
             // Mapping for MobileDeviceId
-            this.Property(c => c.MobileDeviceId).HasColumnName("Id");
+            this.Property(c => c.MobileDeviceId).HasColumnName(MobileDeviceIdColumn);
             this.HasKey(c => c.MobileDeviceId);
             this.Property(c => c.MobileDeviceId).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
             this.Property(c => c.MobileDeviceId).IsRequired();
 
             // Mapping for SerialNo
-            this.Property(c => c.SerialNo).HasColumnName("SerialNo");
+            this.Property(c => c.SerialNo).HasColumnName(SerialNumberColumn);
             this.Property(c => c.SerialNo).IsRequired();
 
             // Mapping for Created
-            this.Property(c => c.Created).HasColumnName("Created");
+            this.Property(c => c.Created).HasColumnName(CreatedColumn);
             this.Property(c => c.Created).IsRequired();
 
             // Mapping for CompanyId
-            this.Property(c => c.CompanyId).HasColumnName("CompanyId");
+            this.Property(c => c.CompanyId).HasColumnName(CompanyIdColumn);
             this.Property(c => c.CompanyId).IsRequired();
         }
     }
